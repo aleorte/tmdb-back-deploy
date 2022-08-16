0415ef34877cdd7cac405b575f28b9bfe26ed961
aleorte <alee_f_ortega012@hotmail.com>
2022-08-16 10:36:25 -0300

    dep

--- db.js
+++ db.js
@@ -1,9 +1,22 @@
 const { Sequelize } = require("sequelize");
 
-const db = new Sequelize("tmdb", null, null, {
-  host: "localhost",
-  dialect: "postgres",
-  logging: false,
-});
+let db
+
+if (process.env.NODE_ENV === 'production') {
+  db= new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+      ssl:{
+        rejectUnauthorized: false
+      }
+    }
+  })
+}
+if(process.env.NODE_ENV === 'development'){
+    db = new Sequelize('tmdb', null, null, {
+    host: 'localhost',
+    dialect: 'postgres',
+    logging: false,
+  });}
+
 
 module.exports = db;
